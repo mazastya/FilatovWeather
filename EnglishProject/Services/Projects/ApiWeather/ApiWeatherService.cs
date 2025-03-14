91735ad935dcb2dfb5a623366ef544a44a358b17
mazastya <amazanova800@gmail.com>
2025-03-14 21:09:29 +0300

    improve ApiWeatherService.cs

--- EnglishProject/Services/Projects/ApiWeather/ApiWeatherService.cs
+++ EnglishProject/Services/Projects/ApiWeather/ApiWeatherService.cs
@@ -8,15 +8,34 @@ public class ApiWeatherService(HttpClient httpClient, IConfiguration configurati
     private readonly string _apiKey = configuration["ApiWeather:ApiKey"]
                                       ?? throw new InvalidOperationException("API key is not set in the configuration.");
 
-    public async Task<WeatherModel> GetWeatherAsync(string city, bool airQuality)
+    public async Task<WeatherModel?> GetWeatherAsync(string city, bool airQuality)
     {
         var airQualityString = airQuality ? "yes" : "no";
         var url = $"https://api.weatherapi.com/v1/current.json?key={_apiKey}&q={city}&aqi={airQualityString}";
 
-        var response = await httpClient.GetAsync(url);
-        response.EnsureSuccessStatusCode();
+        try
+        {
+            var response = await httpClient.GetAsync(url);
 
-        var json = await response.Content.ReadAsStringAsync();
-        return JsonConvert.DeserializeObject<WeatherModel>(json) ?? throw new InvalidOperationException();
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Weather API request failed: {response.StatusCode}, Details: {errorMessage}");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<WeatherModel>(json);
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"HTTP request error: {ex.Message}");
+            return null;
+        }
+        catch (JsonException ex)
+        {
+            Console.WriteLine($"JSON parsing error: {ex.Message}");
+            return null;
+        }
     }
+
 }
